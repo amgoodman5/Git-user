285196b6f6f774a5f8be3eafde76b3930702d399
Dan Levy <dan@danlevy.net>
2017-10-15 23:44:19 -0600

    working form interface & search

--- src/App.js
+++ src/App.js
@@ -8,28 +8,44 @@ class App extends Component {
   constructor(props) {
     super(props);
     this.state = {  };
+    this.findUser = this.findUser.bind(this);
+  }
+
+  findUser(username) {
+    console.log('App.findUser:', username)
+    if (username && username.length >= 3) {
+      fetch(`https://api.github.com/users/${username}`)
+      .then(res => res.json())
+      .then(user => this.setState({username, user: user})) // set user data here
+      .catch(err => {
+        console.log('USER HTTP ERROR', err)
+        this.setState({message: err.message})
+      })
+    } else {
+      this.setState({message: 'Enter valid username'})
+    }
   }
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">Find Git User</h1>
         </header>
         <div className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+          <h4>{this.state.message}</h4>
           <Search
             username={this.state.username}
             onSearch={username => {
               console.log('App.onSearch', username)
-              this.setState((prevState, props) => {
-                let s = {...prevState, username }
-                console.log('App.onSearch.setState()', s)
-                return s;
-              });
+              this.findUser(username)
+              // this.setState((prevState, props) => {
+              //   let s = {...prevState, username }
+              //   console.log('App.onSearch.setState()', s)
+              //   return s;
+              // });
             }}
           />
-          <Card username={this.state.username} />
+          <Card user={this.state.user} />
         </div>
       </div>
     );
--- src/Card.js
+++ src/Card.js
@@ -2,44 +2,16 @@ import React from 'react';
 
 //component did mount
 
-class Card extends React.Component {
-  constructor(props) {
-    super(props);
-    console.log('Card.constructor:', props)
-    this.state = {
-      user: {},
-      username: props.username,
-      message: 'Ready, enter username' };
+function Card({user}) {
+  if (!user || !user.name || !user.login) {
+    return <div className="statusMessage">Search a valid user</div>
   }
-
-  componentDidMount(){
-    console.log('Card.componentDidMount:', this.state)
-    if (this.state.username && this.state.username.length >= 3) {
-      fetch('https://api.github.com/users/' + this.state.username)
-      .then(res => res.json())
-      .then(user => this.setState({...this.state, user: user })) // component
-      .catch(err => {
-        console.log('FAIL HTTP', err)
-        this.setState({...this.state, message: err.message})
-      })
-    } else {
-      this.setState({...this.state, message: 'Ready, enter username'})
-    }
-  }
-
-  render() {
-    console.log('Card.render:', this.state)
-
-    if (this.state.message) {
-      return <div className="statusMessage">Message: {this.state.message}</div>
-    }
-    return (
-      <div className="Card">
-        <img alt={this.state.user.name + ' avatar'} src={this.state.user.avatar_url}/>
-        <h3>{this.state.user.name}</h3>
-      </div>
-    );
-  }
+  return (
+    <div className="card">
+      <h3>{user.name || user.login}</h3>
+      <img alt={user.name + ' avatar'} src={user.avatar_url}/>
+    </div>
+  )
 }
 
 export default Card;
